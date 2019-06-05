cb8c33cb6fc9377efd09a4c89f332c3e4079ed11
Yegor G <yegorich_555@mail.ru>
2019-06-05 18:15:38 +0300

    Receiver. Implement general logic

--- atmel_app/Receiver/Receiver.c
+++ atmel_app/Receiver/Receiver.c
@@ -14,20 +14,43 @@
 #define USOFT_BAUD 1200
 #define USOFT_IO_RX B, 4
 #define USOFT_RXEN 1
+#define USOFT_BUFFER_EN 0
 
 #if DEBUG
 #define USOFT_TXEN 1
 #define USOFT_IO_TX B, 1
 #else
-#define USOFT_TXEN 1
+#define USOFT_TXEN 0
 #endif
 #include <uart_soft.h> //add ref to Atmel-Library and reload Studio
+
+#include "../parcel.h"
+const char parcel[] = STR_PARCEL;
+
+unsigned char myByte;
+bool isNeedTrigger;
 
 int main(void)
 {
 	usoft_init();
+	io_set(DDR, IO_OutSwitch);
+	
 	while (1)
 	{
+		usoft_listen();
+		
+		if (isNeedTrigger)
+		{
+			io_togglePort(IO_OutSwitch);
+			isNeedTrigger = false;
+			delay_ms(DELAY_between);
+		}
 	}
 }
 
+USOFT_ISR_newByte(b)
+{
+	int size = sizeof parcel;
+	myByte = b;
+	//todo analyze of the parcel
+}
--- atmel_app/parcel.h
+++ atmel_app/parcel.h
@@ -1,1 +1,1 @@
-#define STR_PARCEL "Gol2673d"
+#define STR_PARCEL "Gol2673d\n" //parcel must contain end-line character '\n'
