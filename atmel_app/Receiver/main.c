93d56e36ebe4cd3dc8cc33a1b7c520887fec496d
Yegor G <yegorich_555@mail.ru>
2019-06-05 17:20:51 +0300

    Init basic solution

---
+++ atmel_app/Receiver/main.c
@@ -0,0 +1,18 @@
+/*
+ * Receiver.c
+ *
+ * Created: 05.06.2019 17:19:48
+ * Author : YegorVM
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ atmel_app/Transmitter/main.c
@@ -0,0 +1,18 @@
+/*
+ * Transmitter.c
+ *
+ * Created: 05.06.2019 17:18:56
+ * Author : YegorVM
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
