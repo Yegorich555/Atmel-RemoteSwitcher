04b0316994e589ff26fbca56a92f10cbeb34236c
Yegor <yegorich_555@mail.ru>
2019-07-26 22:07:05 +0300

    Transmitter. Add voltage-low indication

--- atmel_app/Transmitter/transmitter.c
+++ atmel_app/Transmitter/transmitter.c
@@ -16,9 +16,43 @@
 
 #include "../parcel.h"
 
+#define REF_AVCC (0<<REFS0) // reference = AVCC
+#define REF_INT  (1<<REFS0) // internal reference 1.1 V
+
+#define ADC_VREF_TYPE REF_INT//((1<<REFS0) | (0<<ADLAR))
+#define IO_ADC_V 3 //port b.3
+
+#define MIN_VOLTAGE 3.8 //Voltage when we need to switch on the LED
+#define Rd1 4700
+#define Rd2 1000
+#define MIN_VOLTAGE_Rd MIN_VOLTAGE*Rd2/(Rd1+Rd2) //The real voltage in voltage divider
+#define MIN_VOLTAGE_ADC MIN_VOLTAGE_Rd*1023 / 1.1 //1023 is max of ADC, 1.1V is the real value of maxADC
+
+#define IO_LED B, 1
+
+void adc_init(void)
+{
+	ADCSRA = (1<<ADEN)|(0<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); // prescaler 8 => 75 kHz
+	DIDR0 |= (0<<ADC0D)|(0<<ADC1D)|(0<<ADC2D)|(1<<ADC3D); //disable digital input for selected ADC
+}
+
+// Read the AD conversion result
+unsigned int read_adc(unsigned char adc_input)
+{
+	ADMUX=adc_input | ADC_VREF_TYPE;
+	_delay_us(10); // Delay needed for the stabilization of the ADC input voltage
+	ADCSRA|=(1<<ADSC); // Start the AD conversion
+	while ((ADCSRA & (1<<ADIF))==0); // Wait for the AD conversion to complete
+	ADCSRA|=(1<<ADIF);
+	return ADCW;
+}
+
+const uint8_t adcCount = 5;
 int main(void)
 {
+	adc_init();
 	usoft_init();
+	
 	while (1)
 	{
 		for (uint8_t i = 0; i < PARCEL_SIZE; ++i)
@@ -26,6 +60,20 @@ int main(void)
 			usoft_putCharf(PARCEL_CHAR);
 		}
 		delay_ms(300);
+		
+		//voltage indicator
+		unsigned int voltage = 0;
+		for (uint8_t i = 0; i < adcCount; ++i)
+		{
+			_delay_us(100);
+			voltage += read_adc(IO_ADC_V);
+		}
+		voltage /= adcCount;
+		
+		if (voltage < MIN_VOLTAGE_ADC){
+			io_set(DDR, IO_LED);
+			io_togglePort(IO_LED);
+		}
 	}
 }
 
